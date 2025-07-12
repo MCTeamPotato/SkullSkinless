1cb9025299618b6f69b1937b5327f6cd01f13ea4
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-12 18:32:52 +0800

    1182

--- src/main/java/me/kall/skullskinless/mixin/item/FillPlayerHeadMixin.java
+++ src/main/java/me/kall/skullskinless/mixin/item/FillPlayerHeadMixin.java
@@ -2,7 +2,6 @@ package me.kall.skullskinless.mixin.item;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.functions.FillPlayerHead;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,8 +9,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(FillPlayerHead.class)
 public abstract class FillPlayerHeadMixin {
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"))
-    private Item noSkin(ItemStack instance) {
-        return Items.AIR;
+    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+    private boolean noSkin(ItemStack instance, Item item) {
+        return false;
     }
 }
